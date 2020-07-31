48b881c8bbcf8c18a5afffabee910b8513f500c5
Laptop <Laptop@example.com>
2020-07-31 19:14:07 +0300

    strip added

--- avitoScrap.py
+++ avitoScrap.py
@@ -1,21 +1,23 @@
 import requests
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 import csv
 
 search_word = 'ps4'
-url = 'https://www.avito.ru/ramenskoe/igry_pristavki_i_programmy?q={}&p=1'.format(search_word) # сегодня
-page  = requests.get(url)
-soup  = BeautifulSoup(page.content.decode('utf-8','ignore'), 'html.parser')
+url = 'https://www.avito.ru/ramenskoe/igry_pristavki_i_programmy?q={}&p=1'.format(search_word)  # сегодня
+page = requests.get(url)
+soup = BeautifulSoup(page.content.decode('utf-8', 'ignore'), 'html.parser')
 
 t = []
 titles = soup.find_all('a', class_='snippet-link')
 for title in titles:
-    t.append(title.get_text('title'))
+    t.append(title.get_text('title', strip=True))
+print(t)
 
 p = []
 prices = soup.find_all('span', class_='snippet-price')
 for price in prices:
     p.append(price.get_text(strip=True))
+# print(p)
 
 l = []
 links = soup.find_all('a', class_='snippet-link')
@@ -23,10 +25,21 @@ for link in links:
     link = 'https://www.avito.ru' + link.get('href')
     l.append(link)
 
-d = zip(t,p,l)
+d = zip(t, p, l)
 result_list = list(d)
 
-print(l)
 with open('{}.csv'.format(search_word), 'w', encoding='utf-8') as f:
     writer = csv.writer(f)
-    writer.writerows(zip(t,p,l))
+    writer.writerows(zip(t, p, l))
+
+# with open('test4pandas.csv', 'w', encoding='utf-8') as f:
+#     writer = csv.writer(f, delimiter='\t')
+#     writer.writerows(zip(t, p))
+
+# with open('test4pandas.txt', 'w', encoding='utf-8', newline=None) as f:
+#     for x, y in zip(t, p):
+#         f.write('{}\t{}'.format(x, y))
+
+# for x, y in zip(t, p):
+#     # print('{}{}'.format(x, y))
+#     print(x, y, '\n')
